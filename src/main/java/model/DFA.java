40feef76e24a02f5f67eca608b63c676327ba845
Kai Sheng <kaisheng62@yahoo.com>
2023-01-05 19:24:43 +0800

    Added the input checking in DFA

--- src/main/java/model/DFA.java
+++ src/main/java/model/DFA.java
@@ -4,59 +4,6 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class DFA extends Automaton {
-    @Override
-    public String getDefinition() {
-        StringBuilder definition = new StringBuilder("M = {Q, Σ, δ, q₀, F}\n");
-        definition.append("Q = ")
-                .append(states.toString().replace("[", "{").replace("]", "}\n"));
-        definition.append("Σ = ")
-                .append(symbols.stream().filter(s -> s != 'Ɛ')
-                        .toList().toString()
-                        .replace("[", "{").replace("]", "}\n"));
-        definition.append("δ: Q x Σ\uD835\uDF74 ➞ Pow(Q)\n");
-        definition.append("q₀ = ").append(states.stream().filter(State::isStartState).toList().get(0)).append("\n");
-        definition.append("F = ")
-                .append(states.stream().filter(State::isFinalState)
-                        .toList()
-                        .toString()
-                        .replace("[", "{").replace("]", "}"));
-        return definition.toString();
-    }
-
-    // Using BFS 
-    private void removeUnreachableStates() {
-        // Construct tree from start state 
-        State startState = states.stream().filter(State::isStartState).toList().get(0);
-        // LinkedHashMap<State, HashMap<Character, State>> statesInMinDFA = new LinkedHashMap<>();
-        // Stack<State> statesInMinDFA = new Stack<>();
-        System.out.println("Start state: " + startState);
-        // Initial Transition Table
-        //            0       1
-        //   Z        Z       Z
-        //   A        A       Z
-        //   B        A       Z
-        // 
-        // Result : A, Z
-        ArrayList<State> resultStates = new ArrayList<>();
-        Stack<State> bfsStates = new Stack<>();
-        bfsStates.add(startState);
-
-        while (!bfsStates.isEmpty()) {
-            if (!resultStates.contains(bfsStates.peek())) {
-                State stateInDFA = bfsStates.pop();
-                resultStates.add(stateInDFA);
-                for (Character c : symbols)
-                    bfsStates.addAll(stateInDFA.getTransition(c));
-            } else
-                bfsStates.pop();
-        }
-        this.states = resultStates;
-        System.out.print("Reachable states: ");
-        for (State s : resultStates)
-            System.out.print(s + " ");
-        System.out.println();
-    }
-
     // Minimize DFA
     public void minimize() {
         removeUnreachableStates();
@@ -72,9 +19,51 @@ public class DFA extends Automaton {
         Group group2 = new Group("G2", finalStates);
 
         HopcroftMinimizer minimizer = new HopcroftMinimizer(group1, group2, symbols);
-        ArrayList<Group> groups = minimizer.getGroups();
-        for (Group g : groups) {
-            System.out.println(g + ": " + g.getStates());
+        this.states = minimizer.minimize();
+    }
+
+    public boolean isStringAcceptedOrRejected(String input) {
+        boolean status = false;
+        // start from start state
+        State currentState = states.stream().filter(State::isStartState).findFirst().orElseThrow();
+        for (int i = 0; i < input.length(); i++) {
+            //get character for string, one by one, as a symbol
+            char symbol = input.charAt(i);
+
+            // Only change state if input is not epsilon
+            // And check if the current state has any transition with the symbol
+            if (symbol != 'Ɛ') {
+                //if contains, update currentstate value
+                if (!currentState.getTransition(symbol).isEmpty())
+                    currentState = currentState.getTransition(symbol).iterator().next();
+                else {
+                    status = false;
+                    break;
+                }
+            }
+
+            //if is last character of string, check if current state is final state
+            status = i == input.length() - 1 && currentState.isFinalState();
         }
+        return status;
+    }
+
+    private void removeUnreachableStates() {
+        // Construct tree from start state
+        State startState = states.stream().filter(State::isStartState).toList().get(0);
+        ArrayList<State> resultStates = new ArrayList<>();
+        Stack<State> bfsStates = new Stack<>();
+        bfsStates.add(startState);
+
+        while (!bfsStates.isEmpty()) {
+            if (!resultStates.contains(bfsStates.peek())) {
+                State stateInDFA = bfsStates.pop();
+                resultStates.add(stateInDFA);
+                for (Character c : symbols)
+                    bfsStates.addAll(stateInDFA.getTransition(c));
+            } else
+                bfsStates.pop();
+        }
+        this.states = resultStates;
     }
 }
